d44d4911848b43eacfb763604fb2c7b056f70c21
luksheyyy <silverfox29983@gmail.com>
2025-04-28 20:17:22 +0100

    Update rpgschema.js

--- Database/rpgschema.js
+++ Database/rpgschema.js
@@ -4,15 +4,10 @@ const playerSchema = new mongoose.Schema({
   id: { type: String, unique: true, required: true },
   name: { type: String },
   inventory: {
-    wood: { type: Number, required: true },
-    stone: { type: Number, required: true },
-    iron: { type: Number, required: true },
-    diamonds: { type: Number, required: true },
-    goldenApple: { type: Number, required: true },
-    diamondpickaxe: { type: Number, required: true },
-    ironpickaxe: { type: Number, required: true },
-    stonepickaxe: { type: Number, required: true },
-    woodenaxe: { type: Number, required: true },
+    walletbal: { type: Number, required: true },
+    ref: { type: Number, required: true },
+    level: { type: Number, required: true },
+    miner: { type: Number, required: true },
   },
 });
 
